430edcefe4085aa9b0ab4448ec3cb9f71e28493c
Emircan <Emircan@EMIRACIKEL-NB>
2021-01-23 16:34:03 +0300

    Add project files.

---
+++ MyListWork/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<I,S>
+    {
+        I[] key;
+        S[] value;
+
+        public MyDictionary()
+        {
+            key = new I[0];
+            value = new S[0];
+
+        }
+        public void Add(I keys, S values)
+        {
+            I[] tempKey = key;
+            S[] tempValue = value;
+
+            key = new I[key.Length + 1];
+            value = new S[value.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+                value[i] = tempValue[i];
+            }
+            key[key.Length - 1] = keys;
+            value[value.Length - 1] = values;
+        }
+        public void WriteDictionary()
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.WriteLine(key[i]);
+                Console.WriteLine(value[i]);
+            }
+            
+        }
+
+    }
+}
---
+++ MyListWork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("Meslek", "Elektronik Mühendisliği");
+            myDictionary.Add("Meslek", "Bigisayar Mühendisliği");
+            myDictionary.Add("Meslek", "Makine Mühendisliği");
+            myDictionary.Add("Meslek", "Mekatronik Mühendisliği");
+            myDictionary.WriteDictionary();
+
+        }
+    }
+}
+
